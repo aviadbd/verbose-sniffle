93b242ed7255c1d6a63d43b8695e231b4ff2d133
aviadbd <aviad.bendov@gmail.com>
2023-04-17 21:27:51 +0900

    Create mapped_images.rb

---
+++ _plugins/mapped_images.rb
@@ -0,0 +1,30 @@
+module Jekyll
+  class RenderTimeTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @images = tokens.images
+    end
+
+    def render(context)
+        # see https://leafletjs.com/examples/quick-start/ 
+      <<~END
+      var lat = #{@images[0].latitude}
+      var lng = #{@images[0].longitude}
+  
+      var map = L.map('map').setView([lat, lng], 13);
+  
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
+      END
+
+#       {% for marker in include.markers do %}
+#         /* var marker = */ L.marker([{{ marker.latitude }}, {{ marker.longitude }}]).addTo(map);
+#       {% endfor %}
+    end
+  end
+end
+
+Liquid::Template.register_tag('mapped_images', Jekyll::RenderTimeTag
