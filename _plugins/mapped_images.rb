a600d8d404375788222a0b6a4e3180ca5b274dd7
aviadbd <aviad.bendov@gmail.com>
2023-04-18 01:30:40 +0900

    Update mapped_images.rb

--- _plugins/mapped_images.rb
+++ _plugins/mapped_images.rb
@@ -1,50 +1,36 @@
-require 'key_value_parser'
-require 'shellwords'
-require 'exifr/jpeg'
-
-
 module Jekyll
-  class RenderTimeTag < Liquid::Tag
-    include Jekyll::Filters::URLFilters    
-    
-    def initialize(tag_name, text, tokens)
-      super
-      @text = text
-    end
-
-    def render(context)
-      argv = Shellwords.split @text
-      options = KeyValueParser.new.parse(argv)
-      
-      
-      if options[:images].start_with?("page.") then
-        images = context.registers[:page][options[:images][5..]]
-      else
-        images = context[options[:images]]
-      end
-        
-      @context = context # for the relative_url method
-        
-      markers = images.map { |image| EXIFR::JPEG.new(absolute_url(image)).gps }
-
-        # see https://leafletjs.com/examples/quick-start/ 
+  module MapsFilter
+    def map_marker(input, map)
       <<~END
-      var lat = #{markers[0].latitude}
-      var lng = #{markers[0].longitude}
+      <script>
+      /* var marker = */ L.marker([#{input.latitude}, #{input.longitude}]).addTo(#{map});
+      </script>
+      END
+    end
+    
+    def render_map(input, name)
+      <<~END
+      <div id='#{name}'></div>
+      
+      <script>
+      var lat = #{input.latitude}
+      var lng = #{input.longitude}
   
-      var map = L.map('map').setView([lat, lng], 13);
+      var map = L.map('name').setView([lat, lng], 13);
   
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(map);
+      </script>
       END
-
-#       {% for marker in include.markers do %}
-#         /* var marker = */ L.marker([{{ marker.latitude }}, {{ marker.longitude }}]).addTo(map);
-#       {% endfor %}
+      END
     end
   end
 end
 
-Liquid::Template.register_tag('mapped_images', Jekyll::RenderTimeTag)
+
+Liquid::Template.register_filter(Jekyll::MapsFilter)
+
+
+
