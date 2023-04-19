bdbfc53e4634358b63eb84b5cd05fb0c89eb3652
aviadbd <aviad.bendov@gmail.com>
2023-04-19 18:52:02 +0900

    Update exif_map.rb

--- _plugins/exif_map.rb
+++ _plugins/exif_map.rb
@@ -71,7 +71,7 @@ module Jekyll
       <<~HTML
       <div id='map'></div>
       <script>
-        var map = L.map('map').setView(#{center}, 1);
+        var map = L.map('map').setView(#{center}, 10);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 19,
           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
